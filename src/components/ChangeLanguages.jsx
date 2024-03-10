ee5e85073f324d19fd8acb1ad54eee120c55ece6
Carlos Vertiz <vertizcar@gmail.com>
2024-03-10 12:39:49 -0300

    bug

--- src/components/ChangeLanguages.jsx
+++ src/components/ChangeLanguages.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 
 export function ChangeLanguages() {
   const currentPath = window.location.pathname;
@@ -7,7 +6,7 @@ export function ChangeLanguages() {
     <div className="flex flex-row gap-4 px-4 py-1 w-fit mt-2 ml-6 border-solid border border-indigo-800/80 rounded-xl lg:mt-5">
       <a href="/" className={`hover:text-teal-950	 ${currentPath == "/" ? "text-sky-500	" : ""}`}>EN</a>
       <div className="border-l border-indigo-800/80 my-1"></div>
-      <a href="es" className={`hover:text-teal-950	 ${currentPath == "/es"? "text-sky-500	" : ""}`}>ES</a>
+      <a href="/es" className={`hover:text-teal-950	 ${currentPath == "/es"? "text-sky-500	" : ""}`}>ES</a>
     </div>
   );
 }
